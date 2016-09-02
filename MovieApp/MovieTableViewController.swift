ec84e3f2c6514c63ceed0e750f618437dfe6e31b
Alex Liang <u9211011@gmail.com>
2016-09-02 15:12:28 +0800

    Modify movie description display

--- MovieApp/MovieTableViewController.swift
+++ MovieApp/MovieTableViewController.swift
@@ -13,6 +13,7 @@ class MovieTableViewController: UITableViewController {
     var movieList = ["美國隊長", "鋼鐵人"]
     var movieImageList = ["captain-america", "iron-man"]
     var releaseDateList = ["2016-07-16", "2015-04-11"]
+    var movieDescriptionList = ["《美國隊長》（英語：Captain America: The First Avenger）是一部於2011年上映的美國動作電影，改編自漫威漫畫中的二次大戰期間的超級英雄故事《美國隊長》，由喬·約翰斯頓導演，克里斯·伊凡、湯米·李·瓊斯及雨果·威明主演。此片是漫威漫畫中第二部以3D拍攝的電影，也是漫威電影宇宙系列中的第五部電影。美國先在2011年7月22日上映，而香港則在2011年8月4日上映。", "《鋼鐵人》（英語：Iron Man）是一部2008年上映的美國動作電影，改編自驚奇漫畫（英語：Marvel Comics）中的科幻超級英雄故事《鋼鐵人》，是漫威電影宇宙系列電影中的第一部。該電影由強·法夫洛導演，小勞勃·道尼及葛妮絲·派特洛等人主演。香港、台灣、馬來西亞、中國大陸均在2008年4月30日首映，5月1日才於美國上映。本片續集於2010年《鋼鐵人2》；2013年《鋼鐵人3》全球上映。"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -59,7 +60,7 @@ class MovieTableViewController: UITableViewController {
                 let vc = segue.destinationViewController as! MovieDetailViewController
                 
                 vc.navigationItem.title = movieList[indexPath.row]
-                vc.movieDescription = "《美國隊長》（英語：Captain America: The First Avenger）是一部於2011年上映的美國動作電影，改編自漫威漫畫中的二次大戰期間的超級英雄故事《美國隊長》，由喬·約翰斯頓導演，克里斯·伊凡、湯米·李·瓊斯及雨果·威明主演。此片是漫威漫畫中第二部以3D拍攝的電影，也是漫威電影宇宙系列中的第五部電影。美國先在2011年7月22日上映，而香港則在2011年8月4日上映。"
+                vc.movieDescription = movieDescriptionList[indexPath.row]
                 vc.movieImage = movieImageList[indexPath.row]
             }
         }
